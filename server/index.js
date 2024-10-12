1a3dac7c29a3a083f43a8da4e413f7322770979b
codesChitransh <143285097+codesChitransh@users.noreply.github.com>
2024-10-12 13:37:33 +0530

    connected datavase

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 const app=express();
+
+mongoose
+.connect(process.env.mongo)
+.then(()=>{
+    console.log("connected");
+})
+.catch((err) =>{
+    console.log(err);
+});
 
 app.listen(4000,()=>{
     console.log("listening at 4000")
 })
+
